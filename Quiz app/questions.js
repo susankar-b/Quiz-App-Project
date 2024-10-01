3b486d064a847b7f74b90d81d9ebaaf6770febbc
susankar-b <145178283+susankar-b@users.noreply.github.com>
2024-10-01 21:37:31 +0530

    Add files via upload

---
+++ Quiz app/questions.js
@@ -0,0 +1,112 @@
+let questions =[
+  {
+      numb: 1,
+      question: "What is my D.O.B ?",
+      answer: "23rd Nov 2002",
+      options:[
+          "17th Nov 2002",
+          "23rd Nov 2003",
+          "23rd Nov 2002",
+          "17th Nov 2001"
+      ]
+  },
+  {
+      numb: 2,
+      question: "Where is my Home town ?",
+      answer: "Sabroom",
+      options:[
+          "Santir Bazar",
+          "Belonia",
+          "Sabroom",
+          "Amarpur"
+      ]
+  },
+  {
+      numb: 3,
+      question: "What is my Age ?",
+      answer: "21",
+      options:[
+          "21",
+          "19",
+          "20",
+          "22"
+      ]
+  },
+  {
+      numb: 4,
+      question: "What is Ssnkr's anytime film to watch ?",
+      answer: "Yeh Jawani Hai Dewaani",
+      options:[
+          "Adipurush",
+          "KGF",
+          "DDLJ",
+          "Yeh Jawani Hai Dewaani"
+      ]
+  },
+  {
+      numb: 5,
+      question: "Where Ssnkr recently went for trekking ?",
+      answer: "Uttrakhand",
+      options:[
+          "Nepal",
+          "Uttrakhand",
+          "Delhi",
+          "Arunachal Pradesh"
+      ]
+  },
+  {
+      numb: 6,
+      question: "What is Ssnkr's height ?",
+      answer: "Ssnkr also don't know!",
+      options:[
+          "172 cm",
+          "175 cm",
+          "169 cm",
+          "Ssnkr also don't know!"
+      ]
+  },
+  {
+      numb: 7,
+      question: "If not Engg. what would Ssnkr be ?",
+      answer: "Owner of Counter",
+      options:[
+          "Army",
+          "Doctor",
+          "Owner of Counter",
+          "Cricketer"
+      ]
+  },
+  {
+      numb: 8,
+      question: "What is Ssnkr's nickname ?",
+      answer: "All of the above",
+      options:[
+          "Penthu",
+          "Susu",
+          "Emon",
+          "All of the above"
+      ]
+  },
+  {
+      numb: 9,
+      question: "What Snnkr do in free time ?",
+      answer: "Watch movies",
+      options:[
+          "Watch movies",
+          "Sleep",
+          "Video Games",
+          "Preapre for GATE"
+      ]
+  },
+  {
+      numb: 10,
+      question: "Favourite cricketer ?",
+      answer: "Virat Kohli",
+      options:[
+          "Gautam Gambhir",
+          "Virat Kohli",
+          "MS Dhoni",
+          "BKL Rahul"
+      ]
+  },
+]
